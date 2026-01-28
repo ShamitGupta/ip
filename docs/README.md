7fc74294822b5de82c6112a5a92440e6a4888bb4
ShamitGupta <e1459301@u.nus.edu>
2026-01-28 21:24:13 +0800

    Rename, Greet, Exit

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Shamit User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/Shamit.java
@@ -0,0 +1,13 @@
+public class Shamit {
+    public static void main(String[] args) {
+//        String logo = " ____        _        \n"
+//                + "|  _ \\ _   _| | _____ \n"
+//                + "| | | | | | | |/ / _ \\\n"
+//                + "| |_| | |_| |   <  __/\n"
+//                + "|____/ \\__,_|_|\\_\\___|\n";
+//
+//        System.out.println("Hello from\n" + logo);
+        String name = "Shamit";
+        System.out.println("Hello! I'm "+ name + "\nWhat can I do for you? \nBye. Hope to see you again soon!");
+    }
+}
