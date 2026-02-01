6eb42705b33a0a049132c9c2a2bb962a53a93cb7
ShamitGupta <e1459301@u.nus.edu>
2026-02-01 16:33:25 +0800

    Added echo function and cleaned code

--- src/main/java/Shamit.java
+++ src/main/java/Shamit.java
@@ -1,13 +1,40 @@
+import java.util.Objects;
+import java.util.Scanner;
 public class Shamit {
     public static void main(String[] args) {
-//        String logo = " ____        _        \n"
-//                + "|  _ \\ _   _| | _____ \n"
-//                + "| | | | | | | |/ / _ \\\n"
-//                + "| |_| | |_| |   <  __/\n"
-//                + "|____/ \\__,_|_|\\_\\___|\n";
-//
-//        System.out.println("Hello from\n" + logo);
+        greet();
+        boolean isEnd = false; //this will be true when user types bye
+        String userInput = "";
+
+        while(!isEnd){
+            Scanner obj = new Scanner(System.in);
+            userInput = obj.nextLine();
+
+            if(Objects.equals(userInput, "bye")){
+                isEnd = true;
+                bye();
+            }
+            else{
+                echo(userInput);
+
+            }
+        }
+
+
+    }
+
+    public static void greet(){
         String name = "Shamit";
-        System.out.println("Hello! I'm "+ name + "\nWhat can I do for you? \nBye. Hope to see you again soon!");
+        System.out.println("Hello! I'm "+ name + "\nHow can I help you?");
+    }
+
+    public static void bye(){
+        System.out.println("Bye, please come again!");
+    }
+
+    public static void echo(String content){
+        System.out.println("-----------------------");
+        System.out.println(content);
+        System.out.println("-----------------------");
     }
 }
